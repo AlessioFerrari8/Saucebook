092e9d6330ee63928942a1f268660dc49bbbd669
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-04-22 11:14:45 +0200

    Feat: implement build script for Chrome and Firefox manifests

---
+++ script/build.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+
+const IGNORE = ['build', 'node_modules', '.git', 'script']
+
+// copio la directory
+function copyDir(src, dest) {
+    // uso il modulo fs
+    fs.mkdirSync(dest, { recursive: true })
+    fs.readdirSync(src).forEach(file => {
+        if (IGNORE.includes(file)) return;
+
+        const srcPath = path.join(src, file)
+        const destPath = path.join(dest, file)
+
+        if (fs.statSync(srcPath).isDirectory()) {
+            copyDir(srcPath, destPath)
+        } else {
+            fs.copyFileSync(srcPath, destPath)
+        }
+    })
+}
+
+// chrome - usa manifest.json
+copyDir('.', './build/chrome')
+fs.copyFileSync('manifest.json', './build/chrome/manifest.json')
+
+// freifox merge manifest base + il manifest per firefox
+const base = JSON.parse(fs.readFileSync('manifest.json'))
+const ff = JSON.parse(fs.readFileSync('manifest.firefox.json'))
+const ffManifest = { ...base, ...f };
+
+copyDir('.', '.build/firefox')
+fs.writeFileSync(
+    '.build/firefox/manifst.json',
+    JSON.stringify(ffManifest, null, 2)
+)
+
+// log
+console.log('Build competed -> build/chrome e build/firefox')
